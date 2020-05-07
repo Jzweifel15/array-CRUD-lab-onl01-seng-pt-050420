3a83e28d8f03c5ed1c7ab5d70173d725b83fb59e
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-07 18:10:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,14 +1,14 @@
 def create_an_empty_array
-  []
+  array = []
 end
 
 def create_an_array
-  [1, 2, 3, 4]
+  array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  newArray = [1, 2, 3, 4]
-  newArray.push = 5
+  array = [1, 2, 3, 4]
+  array.push(5)
 end
 
 def add_element_to_start_of_array(array, element)
